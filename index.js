760cb46b3383fb4f9a60d1f689cef534b227b925
KAIF KHAN <kaif8700979251@gmail.com>
2025-07-21 16:34:30 +0530

    fix: socker cors error

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: [ process.env.MAIN_URL ],
     methods: ["GET", "POST"],
   },
 });
